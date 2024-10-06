6db30cac4c9bbe39279daa6807d65805bbf80a12
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-06 16:16:49 +0600

    Create class Employee and added properties

---
+++ Lesson_48/Employee.cs
@@ -0,0 +1,10 @@
+namespace Lesson_48;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string About { get; set; }
+    public int Age { get; set; }
+}
